4e3825ecba22ee86a6a5f13b55b84c02b094f5a4
Dimitar Dishev <dimitar.dishev@ampeco.global>
2022-07-13 18:15:53 +0300

    upd: remove reverse transaction hardcoded stuff

--- src/Message/ReversalRequest.php
+++ src/Message/ReversalRequest.php
@@ -6,7 +6,6 @@ use Omnipay\Common\Message\ResponseInterface;
 
 class ReversalRequest extends AbstractRequest
 {
-    
     /**
      * Get the raw data array for this message. The format of this varies from gateway to
      * gateway, but will usually be either an associative array, or a SimpleXMLElement.
@@ -16,12 +15,13 @@ class ReversalRequest extends AbstractRequest
     public function getData()
     {
         $amount = $this->getAmountInteger();
+
         return [
             'amount' => $amount,
             'trans_id' => $this->getTransactionId(),
         ];
     }
-    
+
     /**
      * Send the request with specified data
      *
@@ -30,10 +30,8 @@ class ReversalRequest extends AbstractRequest
      */
     public function sendData($data)
     {
-        $this->fibank->createTransactionCompletionCaptureRequest(100, '(#09aa987283) Register a new payment method. The amount will be reverted right after the card registration', $data['trans_id']);
-        
-        $response = $this->fibank->reverseTransaction($data['trans_id'], 100);
-        
+        $response = $this->fibank->reverseTransaction($data['trans_id'], $data['amount']);
+
         return $this->createResponse($response);
     }
 }

98be7aeef18fafd54839ce6e42114ca658b1dc7d
Dimitar Dishev <dimitar.dishev@ampeco.global>
2022-07-21 09:16:51 +0300

    add: is sca completed successfully

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -54,7 +54,12 @@ class Response extends AbstractResponse implements RedirectResponseInterface
 
     public function isScaRequired(): bool
     {
-        return @$this->getData()['RESULT'] == 'PENDING' && $this->getData()['3DSECURE'] == 'FAILED';
+        return @$this->data['RESULT'] == 'PENDING' && $this->data['3DSECURE'] == 'FAILED';
+    }
+
+    public function isScaCompletedSuccessfully(): bool
+    {
+        return @$this->data['RESULT'] == 'OK' && $this->data['3DSECURE'] == 'AUTHENTICATED';
     }
 
     public function getMessage()
